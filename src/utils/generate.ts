f78411b7179d367699aa647c30c523520fcd28ee
mmdxiaoxin <782446723@qq.com>
2025-05-05 16:18:57 +0800

    fix: 修正个人资料生成器中的属性名称

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -16,7 +16,7 @@ export class DataGenerator {
     
     // 生成个人资料
     const profile = new Profile();
-    profile.nickname = faker.person.fullName();
+    profile.name = faker.person.fullName();
     profile.avatar = faker.image.avatar();
     profile.phone = faker.phone.number();
     profile.address = faker.location.streetAddress();
